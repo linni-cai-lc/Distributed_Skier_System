690966717292be8361f8a5ee8a1c6597ea5ee10f
LINNI2017 <cai20170607@gmail.com>
2022-02-14 12:52:04 -0800

    finish hw1 client part 1

---
+++ hw1/client/src/main/java/part1/ClientMultiThreaded.java
@@ -0,0 +1,94 @@
+package part1;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.api.SkiersApi;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ClientMultiThreaded {
+//    private static final String PATH = "/hw1_war_exploded/ski"; // LOCAL
+    private static final String PATH = "/hw1_war/ski"; // AWS
+    private static final String HTTP_PREFIX = "http://";
+    private static final double PHASE_PCT = 0.2;
+
+    public static void main(String[] args) throws InterruptedException {
+        // LOCAL --num_threads 64 --num_skiers 128 --num_lifts 40 --num_runs 20 --ip_address 152.44.141.6:8080
+        // AWS   --num_threads 64 --num_skiers 128 --num_lifts 40 --num_runs 20 --ip_address 54.200.234.195:8080
+        CommandLineParser parser = CommandLineParser.parseCommandArgs(args);
+        ApiClient apiClient = new ApiClient();
+        // LOCAL http://152.44.141.6:8080/hw1_war_exploded/ski
+        // AWS   http://54.200.234.195:8080/hw1_war/ski
+        String path = HTTP_PREFIX + parser.ipAddress + PATH;
+        apiClient.setBasePath(path);
+        SkiersApi skiersApi = new SkiersApi(apiClient);
+
+        int numThreads = parser.numThreads;
+        int numSkiers = parser.numSkiers;
+        int numLifts = parser.numLifts;
+        int numRuns = parser.numRuns;
+        String ipAddress = parser.ipAddress;
+
+        int numThreadsPhase1 = numThreads / 4;
+        int numThreadsPhase2 = numThreads;
+        int numThreadsPhase3 =(int)Math.ceil(numThreads * 0.1);
+
+        AtomicInteger numSuccessReq = new AtomicInteger(0);
+        AtomicInteger numUnsuccessReq = new AtomicInteger(0);
+        CountDownLatch totalCompleted = new CountDownLatch(numThreadsPhase1 + numThreadsPhase2 + numThreadsPhase3);
+
+        int numPostsPhase1 = (int)Math.ceil((numRuns * 0.2) * (numSkiers / numThreadsPhase1));
+        int numPostsPhase2 = (int)Math.ceil((numRuns * 0.6) * (numSkiers / numThreadsPhase2));
+        int numPostsPhase3 = (int)Math.ceil(numRuns * 0.1 * numThreadsPhase3);
+
+        // trigger phase 2 when 20% of phase 1 completed;
+        int numTriggerPhase2 = (int)Math.ceil(numThreadsPhase1 * PHASE_PCT);
+        CountDownLatch completedPhase2 = new CountDownLatch(numTriggerPhase2);
+        // trigger phase 3 when 20% of phase 2 completed;
+        int numTriggerPhase3 = (int)Math.ceil(numThreadsPhase2 * PHASE_PCT);
+        CountDownLatch completedPhase3 = new CountDownLatch(numTriggerPhase3);
+
+        int startTimePhase1 = 1;
+        int endTimePhase1 = 90;
+        int startTimePhase2 = 91;
+        int endTimePhase2 = 360;
+        int startTimePhase3 = 361;
+        int endTimePhase3 = 420;
+
+        Phase phase1 = new Phase(numThreadsPhase1, numSkiers, startTimePhase1, endTimePhase1,
+                numLifts, numPostsPhase1, numSuccessReq, numUnsuccessReq, totalCompleted,
+                completedPhase2, skiersApi);
+        Phase phase2 = new Phase(numThreadsPhase2, numSkiers, startTimePhase2, endTimePhase2,
+                numLifts, numPostsPhase2, numSuccessReq, numUnsuccessReq, totalCompleted,
+                completedPhase3, skiersApi);
+        Phase phase3 = new Phase(numThreadsPhase3, numSkiers, startTimePhase3, endTimePhase3,
+                numLifts, numPostsPhase3, numSuccessReq, numUnsuccessReq, totalCompleted,
+                completedPhase3, skiersApi);
+
+
+        // run phase
+        long start = System.currentTimeMillis();
+
+        System.out.println("------  PHASE 1  ------");
+        phase1.start();
+        completedPhase2.await();
+
+        System.out.println("------  PHASE 2  ------");
+        phase2.start();
+        completedPhase3.await();
+
+        System.out.println("------  PHASE 3  ------");
+        phase3.start();
+        totalCompleted.await();
+
+        long end = System.currentTimeMillis();
+
+        long duration = end - start;
+        long throughput = (numSuccessReq.get() + numUnsuccessReq.get()) / (duration / 1000);
+        System.out.println("------  Statistics  ------");
+        System.out.println("number of successful requests sent: " + numSuccessReq);
+        System.out.println("number of unsuccessful requests: " + numUnsuccessReq);
+        System.out.println("the total run time for all phases to complete: " + duration);
+        System.out.println("the total throughput in requests per second: " + throughput);
+    }
+}
---
+++ hw1/client/src/main/java/part1/Phase.java
@@ -0,0 +1,114 @@
+package part1;
+
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiResponse;
+import io.swagger.client.api.SkiersApi;
+import io.swagger.client.model.LiftRide;
+import javax.servlet.http.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Phase {
+    protected static final int MIN_WAIT_TIME = 0;
+    protected static final int MAX_WAIT_TIME = 10;
+    protected static final Integer RESORT_ID = 111;
+    protected static final String SEASON_ID = "WINTER";
+    protected static final String DAY_ID = "112";
+
+    protected int numThreads;
+    protected int numSkiers;
+    protected int startTime;
+    protected int endTime;
+    protected int numLifts;
+    protected int numPosts;
+    protected AtomicInteger numSuccessReq;
+    protected AtomicInteger numUnsuccessReq;
+    protected CountDownLatch totalCompleted;
+    protected CountDownLatch nextPhaseCompleted;
+    protected SkiersApi skiersApi;
+
+    public Phase(int numThreads, int numSkiers, int startTime, int endTime, int numLifts,
+                 int numPosts, AtomicInteger numSuccessReq, AtomicInteger numUnsuccessReq,
+                 CountDownLatch totalCompleted, CountDownLatch nextPhaseCompleted, SkiersApi skiersApi) {
+        this.numThreads = numThreads;
+        this.numSkiers = numSkiers;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.numLifts = numLifts;
+        this.numPosts = numPosts;
+        this.numSuccessReq = numSuccessReq;
+        this.numUnsuccessReq = numUnsuccessReq;
+        this.totalCompleted = totalCompleted;
+        this.nextPhaseCompleted = nextPhaseCompleted;
+        this.skiersApi = skiersApi;
+    }
+
+    private int getRandomSkierId(int threadIndex) {
+        int numSkiersPerThread = numSkiers / numThreads;
+        // threadIndex 0, numSkiersPerThread 100
+        // 100 * 0 + 1, 100 * 1 -> 1, 100
+        // threadIndex 1, numSkiersPerThread 100
+        // 100 * 1 + 1, 100 * 2 -> 101, 200
+        return getRandomNumber(numSkiersPerThread * threadIndex + 1, numSkiersPerThread * (threadIndex + 1));
+    }
+
+    private int getRandomLiftId() {
+        return getRandomNumber(CommandLineParser.MIN_NUM_LIFTS, numLifts);
+    }
+
+    private int getRandomTime() {
+        return getRandomNumber(startTime, endTime);
+    }
+
+    private int getRandomWaitTime() {
+        return getRandomNumber(MIN_WAIT_TIME, MAX_WAIT_TIME);
+    }
+
+    private int getRandomNumber(int minNum, int maxNum) {
+        return (int)(Math.random() * (maxNum - minNum + 1)) + minNum;
+    }
+
+    public void start() {
+        for (int i = 0; i < numThreads; i++) {
+            Runnable runnable = createRunnable(i);
+            Thread thread = new Thread(runnable);
+            thread.start();
+        }
+    }
+
+    private LiftRide createLiftRide() {
+        LiftRide liftRide = new LiftRide();
+        liftRide.setLiftID(getRandomLiftId());
+        liftRide.setTime(getRandomTime());
+        liftRide.setWaitTime(getRandomWaitTime());
+        return liftRide;
+    }
+
+    public Runnable createRunnable(final int threadIndex) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                // POST
+                for (int i = 0; i < numPosts; i++) {
+                    int skierId = getRandomSkierId(i);
+//                    System.out.println(String.format("------- THREAD: %d, POST: %d, ID: %d ---------", threadIndex, i, skierId));
+                    LiftRide liftRide = createLiftRide();
+                    try {
+                        ApiResponse<Void> res = skiersApi.writeNewLiftRideWithHttpInfo(liftRide, RESORT_ID, SEASON_ID, DAY_ID, skierId);
+                        if (res.getStatusCode() == HttpServletResponse.SC_CREATED) {
+                            numSuccessReq.incrementAndGet();
+                        } else {
+                            numUnsuccessReq.incrementAndGet();
+                        }
+                    } catch (ApiException e) {
+                        numUnsuccessReq.incrementAndGet();
+                        e.printStackTrace();
+                    }
+                }
+                totalCompleted.countDown();
+                nextPhaseCompleted.countDown();
+            }
+        };
+    }
+}
